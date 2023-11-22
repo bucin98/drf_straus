207408c5670d070aee8da5b94ddb3e3503483d8c
avallak <bucin98@gmail.com>
2023-11-22 20:04:58 +0200

    Db index

--- shop/models.py
+++ shop/models.py
@@ -14,7 +14,7 @@ class Category(models.Model):
 
 class Product(models.Model):
     name = models.CharField(max_length=200, verbose_name='Наименование товара')
-    category = models.ForeignKey(Category, on_delete=models.CASCADE, verbose_name='Категория')
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, verbose_name='Категория', db_index=True)
     price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name='Цена')
     sold_items_count = models.IntegerField(default=0, verbose_name='Количество проданных товаров')
     created_at = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания')
@@ -25,13 +25,16 @@ class Product(models.Model):
     class Meta:
         verbose_name = 'Товар'
         verbose_name_plural = 'Товары'
+        indexes = [
+            models.Index(fields=['category']),
+        ]
 
 
 class Order(models.Model):
     customer_name = models.CharField(max_length=100, verbose_name='Имя заказчика')
     products = models.ManyToManyField(Product, verbose_name='Товары в заказе')
     total_amount = models.DecimalField(max_digits=10, decimal_places=2, default=0, verbose_name='Общая сумма заказа')
-    order_date = models.DateField(auto_now_add=True, verbose_name='Дата заказа')
+    order_date = models.DateField(auto_now_add=True, verbose_name='Дата заказа', db_index=True)
 
     def __str__(self):
         return f"{self.customer_name}_{self.order_date}"
@@ -39,3 +42,6 @@ class Order(models.Model):
     class Meta:
         verbose_name = 'Заказ'
         verbose_name_plural = 'Заказы'
+        indexes = [
+            models.Index(fields=['order_date']),
+        ]
